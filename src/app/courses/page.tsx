1dd875d41279cc5c3900c527e220a7b5bea69b8c
Swayanshu004 <swayanshusahoo.pri@gmail.com>
2024-07-09 23:39:09 +0530

    Add Updates In Courses Page

--- src/app/courses/page.tsx
+++ src/app/courses/page.tsx
@@ -109,7 +109,7 @@ function page() {
     };
   return (
     <div>
-        <h1 className="text-center text-5xl font-bold mt-20">All Courses [{courseData.courses.length}]</h1>
+        <h1 className="text-center text-5xl font-bold mt-20">All Courses [ {courseData.courses.length} ]</h1>
         <div className="flex flex-wrap justify-center gap-8">
             {
                 courseData.courses.map((item, i)=>( 
@@ -119,23 +119,17 @@ function page() {
                         translateZ="50"
                         className="text-xl font-bold text-neutral-600 dark:text-white"
                         >
-                        Make things float in air
+                        {item.title}
                         </CardItem>
                         <CardItem
                         as="p"
                         translateZ="60"
                         className="text-neutral-500 text-sm max-w-sm mt-2 dark:text-neutral-300"
                         >
-                        Hover over this card to unleash the power of CSS perspective
+                        {item.description}
                         </CardItem>
                         <CardItem translateZ="100" className="w-full mt-4">
-                        <img
-                            src="https://images.unsplash.com/photo-1441974231531-c6227db76b6e?q=80&w=2560&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-                            height="1000"
-                            width="1000"
-                            className="h-60 w-full object-cover rounded-xl group-hover/card:shadow-xl"
-                            alt="thumbnail"
-                        />
+                        {item.image}
                         </CardItem>
                         <div className="flex justify-between items-center mt-20">
                         <CardItem
@@ -143,16 +137,9 @@ function page() {
                             as={Link}
                             href="https://twitter.com/mannupaaji"
                             target="__blank"
-                            className="px-4 py-2 rounded-xl text-xs font-normal dark:text-white"
+                            className="w-full px-4 py-2 rounded-xl text-md font-bold dark:text-orange-600"
                         >
                             Try now →
-                        </CardItem>
-                        <CardItem
-                            translateZ={20}
-                            as="button"
-                            className="px-4 py-2 rounded-xl bg-black dark:bg-white dark:text-black text-white text-xs font-bold"
-                        >
-                            Sign up
                         </CardItem>
                         </div>
                     </CardBody>
